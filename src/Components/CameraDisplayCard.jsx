f8b83d38fea5ebb5c2e214b4f7c2781a51d2072c
cylasion@gmail.com <cylasion@gmail.com>
2025-03-04 08:56:07 +0700

    use ref for camera card display

--- src/Components/CameraDisplayCard.jsx
+++ src/Components/CameraDisplayCard.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import './css/CameraDisplayCard.css';
 
 const CameraDisplayCard = ({ imageUrl, name, description }) => {
+  const streamRef = useRef(null);
+
+  useEffect(() => {
+    if (imageUrl && streamRef.current) {
+      streamRef.current.src = imageUrl;
+    }
+  }, [imageUrl]);
+
   return (
     <div className="camera-display-card">
       <div className="image-container">
-        <img src={imageUrl} alt={name} className="image" />
+        <img ref={streamRef} src={imageUrl} alt={name} className="image" />
       </div>
       <div className="info-container">
         <span className="image-name">{name}</span>
